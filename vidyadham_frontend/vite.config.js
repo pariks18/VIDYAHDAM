b27a78778476fad6fe86a0029260fbb309694777
Parikshitgole <parikshitgole@Parikshitgoles-MacBook-Air.local>
2026-04-20 23:07:04 +0530

    frontend update

--- vidyadham_frontend/vite.config.js
+++ vidyadham_frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5003',
+        target: 'https://vidyahdam-backend.onrender.com',
         changeOrigin: true,
       },
     },
